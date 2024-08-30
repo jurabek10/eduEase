094421df76e3335ebee4dcacd79bbd027b3f72f5
jurabek10 <samiev.jurabek@bk.ru>
2024-08-31 03:16:50 +0900

    feat: integration of express

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
+import app from "./app";
 
-console.log(process.env.PORT);
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.info(`The server is running successfully on port: ${PORT}`);
+      console.info(`Admin projects on http://localhost:${PORT}/admin \n`);
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR on connection MongoDB", err);
+  });
